725b6121fd36ac8ff8c58e0fa29c1bd1d691221e
Will Li <will.li@happyhackers.com.au>
2018-07-28 15:46:16 +1000

    add login page

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,9 +3,16 @@
 
 
 module.exports = {
+    // Index Page
     indexPage: function (req, res) {
         res.render('index.ejs', {
             // user: req.user,
         });
-    }
+    },
+    // Login Page
+    loginPage: function (req, res) {
+        res.render('login.ejs', {
+            // message: req.flash('loginMessage'),
+        });
+    },
 }
