dcbc0ce0b738913cd6e64c63fe71aa0ee8dedd0b
Will Li <will.li@happyhackers.com.au>
2018-07-28 19:10:54 +1000

    changed login and signup page style

--- routes/router.js
+++ routes/router.js
@@ -8,7 +8,16 @@ router.get('/index', controller.indexPage);
 
 
 
-// log in
+//Sign up
+router.get('/signup', controller.signupPage);
+// router.post('/signup', passport.authenticate('local-signup', {
+//     successRedirect: '/index', // redirect to the secure profile section
+//     failureRedirect: '/signup', // redirect back to the signup page if there is an error
+//     failureFlash: true // allow flash messages
+// }));
+
+
+// Log in
 router.get('/login', controller.loginPage);
 // router.post('/login', passport.authenticate('local-login', {
 //     successRedirect: '/index', // redirect to the secure profile section
